39a745af7fd91d9c8b91e7d00b6f393d8feb1287
jota3275 <josefernandez3275@gmail.com>
2024-12-07 17:44:41 -0300

    v.0.0.5

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";  // Importa getFirestore
-// Configuración de Firebase
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBTHiSZoO70essS_yPXEiPB4Lh45O4i9oA",
     authDomain: "jotashop-b4d74.firebaseapp.com",
@@ -10,8 +10,8 @@ const firebaseConfig = {
     appId: "1:1096016592803:web:5164967c1bc1bebad4a416"
 };
 
-// Inicializa Firebase
+
 const app = initializeApp(firebaseConfig);
-// Asegúrate de obtener correctamente la instancia de Firestore
+
 const db = getFirestore(app);
 export { db };
